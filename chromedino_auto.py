6aef5037640177027d44749ce61ceb79bc5ed13b
Saul Figueroa <35702217+sfiguero-git@users.noreply.github.com>
2024-02-15 12:55:30 -0500

    Uploaded main files

---
+++ chromedino_auto.py
@@ -0,0 +1,39 @@
+import pyautogui as pag #pip install pyautogui
+import time  
+import sys   
+
+#@author Saul Figueroa.
+
+print('''
+       _                                  _ _                         _        
+      | |                                | (_)                       | |       
+   ___| |__  _ __ ___  _ __ ___   ___  __| |_ _ __   ___   __ _ _   _| |_ ___  
+  / __| '_ \| '__/ _ \| '_ ` _ \ / _ \/ _` | | '_ \ / _ \ / _` | | | | __/ _ \ 
+ | (__| | | | | | (_) | | | | | |  __/ (_| | | | | | (_) | (_| | |_| | || (_) |
+  \___|_| |_|_|  \___/|_| |_| |_|\___|\__,_|_|_| |_|\___/ \__,_|\__,_|\__\___/                 
+                                                     |______| 
+
+                                                               ~Saul was here...                 
+      ''')
+            
+print('The script is running...\n')
+print('Press CTRL+C to stop!')
+image = 'images/dino.png'
+confidenceVal = 0.90      #For image location.
+
+#For reducing the size of the region for image identification:
+regionLimitatorX = 1001
+regionLimitatorY = 251     
+
+while(1): #The program runs until we press CTRL+C.   
+    try:
+        coordinate = pag.locateCenterOnScreen(image, confidence = confidenceVal, region=(0,0, 1920-regionLimitatorX, 1080-regionLimitatorY)) #pip install opencv-python
+          
+        if coordinate == None:
+            time.sleep(0.046) 
+            pag.press('space') 
+    
+    except KeyboardInterrupt: #CTRL+C
+        print('\nBye!\n')
+        sys.exit()
+	    
